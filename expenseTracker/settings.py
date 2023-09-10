40223a60254784bdadc98449b47fec7887d35594
obewas <obewas1202@gmail.com>
2023-09-10 08:41:24 +0300

    created .gitignore file

--- expenseTracker/settings.py
+++ expenseTracker/settings.py
@@ -78,12 +78,13 @@ WSGI_APPLICATION = 'expenseTracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': os.getenv('ENGINE'),
         'NAME': os.getenv('NAME'),
         'USER': os.getenv('USER'),
         'PASSWORD': os.getenv('PASSWORD'),
     }
 }
+
 
 
 
